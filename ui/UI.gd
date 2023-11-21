692c9cbac5fc77f4725e431e8fbd654523b53af1
DevDrache <dominik.mast.11@gmail.com>
2023-11-21 16:54:35 +0300

    change ui Layout

--- ui/UI.gd
+++ ui/UI.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+@onready var cherryLabel = $Control/MarginContainer/HBoxContainer/CherryContainer/Label
+@onready var goldCoinLabel = $Control/MarginContainer/HBoxContainer/GoldContainer/Label
 
 func _ready():
 	LevelManager.gained_cherry.connect(update_cherry_display)
@@ -8,19 +10,19 @@ func _ready():
 	LevelManager.gained_gold_coin.connect(update_gold_coin_display)
 
 func update_cherry_display(cherries):
-	$CherryCount/Label.text = "x " + str(cherries)
+	cherryLabel.text = "x " + str(cherries)
 
 func update_gem_display(gems):
 	for i in range(gems):
-		var path = "GemCounter/gem" + str(i+1)
+		var path = "Control/MarginContainer/HBoxContainer/GemContainer/Gem" + str(i+1)
 		var gemRect = get_node(path)
 		gemRect.set_modulate(Color(1,1,1))
 
 func update_red_coin_display(coins):
 	for i in range(coins):
-		var path = "RedCoinCounter/redCoin" + str(i+1)
+		var path = "Control/MarginContainer/HBoxContainer/RedCoinContainer/RedCoin" + str(i+1)
 		var gemRect = get_node(path)
 		gemRect.set_modulate(Color(1,1,1))
 
 func update_gold_coin_display(coins):
-	$GoldCoinCounter/Label.text = "x " + str(coins)
+	goldCoinLabel.text = "x " + str(coins)
