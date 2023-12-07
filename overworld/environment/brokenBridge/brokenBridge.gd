4eec5a455da9f5a57ea97b95d683f0b3e16b9836
DevDrache <dominik.mast.11@gmail.com>
2023-12-07 12:35:17 +0200

    add broken bridge

--- overworld/environment/brokenBridge/brokenBridge.gd
+++ overworld/environment/brokenBridge/brokenBridge.gd
@@ -1,14 +1,21 @@
 extends StaticBody2D
 
+@export var iceBridge = false
 
 var isComplete = false
+var bridgeSprite : Sprite2D
+
+func _ready():
+	if iceBridge: bridgeSprite = $iceBridgeBody
+	else: $bridgeBody
+
 		
 func _process(_delta):
-	if isComplete: $bridgeBody.visible = true
+	if isComplete: bridgeSprite.visible = true
 		
 
 func _on_area_2d_body_entered(body):
 	if body is Duck:
 		await get_tree().create_timer(1).timeout
-		$bridgeBody.visible = true
+		bridgeSprite.visible = true
 		$CollisionShape2D.disabled = true
