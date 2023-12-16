ef6feea7599280cbe54ac88bd8fd888dec9b661c
DevDrache <dominik.mast.11@gmail.com>
2023-12-16 08:22:21 +0200

    add multi use

--- level/environment/switch/switch.gd
+++ level/environment/switch/switch.gd
@@ -1,8 +1,14 @@
 extends Area2D
+class_name Switch
+
+@export var multiUse = false
 
 var used = false
 
 func _on_body_entered(body):
 	if body is Player:
 		$Sprite2D.frame = 1
-		used = true
+		
+		if multiUse: used = !used
+		else: used = true
+		
