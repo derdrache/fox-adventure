ff22d84d991a6807ec3138f392544e4be42c20e4
DevDrache <dominik.mast.11@gmail.com>
2023-12-19 12:55:56 +0200

    add secret red coin

--- level/items/goldCoins/GoldCoin.gd
+++ level/items/goldCoins/GoldCoin.gd
@@ -1,7 +1,15 @@
 extends Area2D
 
+@export var hasRedCoin = false
+
 
 func _on_body_entered(body):
 	if body is Player:
-		LevelManager.gain_gold_coin(1)
-		queue_free()	
+		if hasRedCoin:
+			$RedCoinSprite.visible = true
+			await get_tree().create_timer(0.5).timeout
+			LevelManager.gain_red_coin(1)
+			queue_free()
+		else:
+			LevelManager.gain_gold_coin(1)
+			queue_free()	
