1d0df0615aa5834221e3ba25a47b0ae1f6207ad6
DevDrache <dominik.mast.11@gmail.com>
2023-12-19 12:56:09 +0200

    remove camera limiter

--- level/player/Camera2D.gd
+++ level/player/Camera2D.gd
@@ -8,7 +8,7 @@ func _ready():
 	var mapRect = levelTileMap.get_used_rect()
 	var tileSize = levelTileMap.cell_quadrant_size
 	var worldSizeInPixel = mapRect.size * tileSize
-	limit_left = 0
-	limit_top = 100
-	limit_right = worldSizeInPixel.x
+	#limit_left = 0
+	#limit_top = 100
+	#limit_right = worldSizeInPixel.x
 
