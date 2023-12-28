7901e665387ba2c5f24776b08e3dceee981ef071
DevDrache <dominik.mast.11@gmail.com>
2023-12-28 12:55:31 +0200

    fix multi use sprite

--- level/environment/switch/switch.gd
+++ level/environment/switch/switch.gd
@@ -6,6 +6,11 @@ class_name Switch
 
 func _ready():
 	if used: $Sprite2D.frame = 1
+	
+	
+func _process(delta):
+	if used: $Sprite2D.frame = 1
+	else: $Sprite2D.frame = 0
 
 func _on_body_entered(body):
 	if body is Player:
