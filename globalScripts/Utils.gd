cd378fe687d879e67ea7e478683b2cfe629a1548
DevDrache <dominik.mast.11@gmail.com>
2023-12-31 06:40:16 +0200

    clean code

--- globalScripts/Utils.gd
+++ globalScripts/Utils.gd
@@ -2,8 +2,6 @@ extends Node
 
 const SAVE_PATH = "res://savegame.bin"
 
-
-
 func save_game():
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
 	var data : Dictionary = {
@@ -20,8 +18,7 @@ func load_game():
 	var file = FileAccess.open(SAVE_PATH, FileAccess.READ)
 	
 	if FileAccess.file_exists(SAVE_PATH):
-		if not file.eof_reached():
-			var data = JSON.parse_string(file.get_line())
-			GameManager.levelDetails = data["levelDetails"]
-			GameManager.playerPosition = str_to_var(data["playerPosition"])
+		var data = JSON.parse_string(file.get_line())
+		GameManager.levelDetails = data["levelDetails"]
+		GameManager.playerPosition = str_to_var(data["playerPosition"])
 
