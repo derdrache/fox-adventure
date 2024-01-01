13eb4e56d7a1ad59f3ca9a09e563adf70dbdfe8d
DevDrache <dominik.mast.11@gmail.com>
2024-01-01 18:22:59 +0200

    change platforms to Sprite

--- level/environment/moving_objects/moving_platform.gd
+++ level/environment/moving_objects/moving_platform.gd
@@ -1,9 +1,7 @@
 extends Path2D
 
-@onready var pathFollow : PathFollow2D = $PathFollow2D
-
 @export var moving_speed = 50
 
 func _physics_process(delta):
-	pathFollow.progress += moving_speed * delta
+	$PathFollow2D.progress += moving_speed * delta
 	
