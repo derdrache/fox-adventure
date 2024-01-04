6068096e16a9c4db6090092fb1e83d375f718ed0
DevDrache <dominik.mast.11@gmail.com>
2024-01-04 10:11:58 +0200

    clean code

--- level/environment/moving_objects/eagle.gd
+++ level/environment/moving_objects/eagle.gd
@@ -8,66 +8,73 @@ class_name Eagle
 @onready var sleepText = $PathFollow2D/eagleBody/RichTextLabel
 
 @export var isActive = true
-@export var flip_h = false
+@export var flipH = false
 
 
 const MOVE_SPEED = 100
 
 var move = false
 var playerBody : CharacterBody2D
-var ridePosition = Vector2.ZERO
+var enteredBottom = false
 
 func _ready():
-	if flip_h: 
-		eagleSprite.flip_h = true
-		ridePosition.x = 5
+	if flipH: eagleSprite.flip_h = true
 	
-	if isActive:
-		animationPlayer.play("idle")
+	if isActive:animationPlayer.play("idle")
 	else:
 		sleepText.visible = true
 		eagleSprite.frame = 1
 
 
 func _physics_process(delta):
-	if !move && path_follow.progress > 0 && playerBody == null: 
-		resetPosition(delta)
 	
+	_resetPosition(delta)
+	
+	_moving(delta)
+		
+	_bottomRealse()
+	
+func _resetPosition(delta):
+	if !move && path_follow.progress > 0 && playerBody == null: 
+		path_follow.progress -= MOVE_SPEED * delta
+	
+func _moving(delta):	
 	if move && playerBody != null: 
+		var ridePosition = Vector2(
+			5 if flipH else 0, 
+			8 if enteredBottom else -10)
+		
 		path_follow.progress += MOVE_SPEED * delta
 		var newPosition = eagleBody.global_position
 		newPosition += ridePosition
 		playerBody.position = newPosition
+
+func _bottomRealse():
+	var pressRealseButtons = (Input.is_action_pressed("ui_accept") 
+		|| Input.is_action_pressed("move_down"))
+	var isMoving = path_follow.progress > 1	
+	
+	if enteredBottom && pressRealseButtons && isMoving: move = false
 		
-	if path_follow.progress_ratio == 1: 
-		move = false
-	
-func resetPosition(delta):
-	path_follow.progress -= MOVE_SPEED * delta
-	
 func _on_area_top_body_entered(body):
 	if body is Player && isActive:
-		body.onMoveableObject = true
-		ridePosition.y = -10
 		playerBody = body
 		move = true
+		enteredBottom = false
 
 func _on_area_top_body_exited(body):
 	if body is Player && isActive:
-		body.onMoveableObject = false
 		move = false
 		playerBody = null
 
 func _on_area_bottom_body_entered(body):
 	if body is Player && isActive:
-		body.onMoveableObject = true
 		playerBody = body
 		move = true
-		ridePosition.y = 8
+		enteredBottom = true
 
 func _on_area_bottom_body_exited(body):
 	if body is Player && isActive: 
-		body.onMoveableObject = false
 		move = false
 		playerBody = null
 
