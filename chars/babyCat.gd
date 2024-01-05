de7ed7641f782721902fc4a9743b8df75f1190f7
DevDrache <dominik.mast.11@gmail.com>
2024-01-05 14:54:27 +0200

    rename

--- chars/babyCat.gd
+++ chars/babyCat.gd
@@ -1,15 +1,26 @@
 extends CharacterBody2D
 
-@onready var animationSprite = $AnimatedSprite2D
+@onready var whiteCatAnimationsSprite = $whiteCat
+@onready var yellowCatAnimationsSprite = $yellowCat
+@onready var BlackCatAnimationsSprite = $blackCat
+@onready var brownCatAnimationsSprite = $brownCat
+@onready var blueCatAnimationsSprite = $blueCat
+@onready var berryCatAnimationsSprite = $berryCat
 
 @export var target: Player
+@export var isWaiting = true
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var animationSprite : AnimatedSprite2D
 
+
+func _ready():
+	whiteCatAnimationsSprite.visible = false
+	_selectCatColor()
 
 func _physics_process(delta):
 	_move()
@@ -19,8 +30,10 @@ func _physics_process(delta):
 	_set_animation()
 	
 func _move():
-	var catMultiplicator = int(name.replace("Cat", ""))
-	if catMultiplicator == 0: catMultiplicator = 1
+	if isWaiting: return
+	
+	var catNumber = int(name.replace("Cat", ""))
+	var catMultiplicator = 1 if catNumber == 0 else catNumber
 
 	var targetPosition = target.position - Vector2(0, -9)
 
@@ -33,6 +46,8 @@ func _move():
 	else: velocity = Vector2.ZERO
 
 func _set_animation():
+	if isWaiting: return
+	
 	var targetPosition = target.position - Vector2(0, -9)
 
 	if velocity.x > 0: animationSprite.flip_h = false
@@ -46,3 +61,18 @@ func _set_animation():
 	elif velocity != Vector2.ZERO:
 		animationSprite.play("walk")
 	else: animationSprite.play("idle")
+
+func _selectCatColor():
+	var catColorArray = [whiteCatAnimationsSprite, yellowCatAnimationsSprite, BlackCatAnimationsSprite, 
+	brownCatAnimationsSprite, blueCatAnimationsSprite, berryCatAnimationsSprite]
+	var rng = RandomNumberGenerator.new()
+	var randomNumber = rng.randi_range(0, 5)
+	
+	animationSprite = catColorArray[randomNumber]
+	animationSprite.visible = true
+
+
+
+func _on_area_2d_body_entered(body):
+	if body is Player && isWaiting:
+		isWaiting = false
