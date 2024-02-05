6f235fdcd6eceee307773d2ab53c10fe39a20f0c
DevDrache <dominik.mast.11@gmail.com>
2024-02-05 14:36:42 +0200

    add play time helper function

--- globalScripts/HelperFunctions.gd
+++ globalScripts/HelperFunctions.gd
@@ -3,3 +3,11 @@ extends Node
 func is_parent_switch(parent):	
 	if parent is Switch || parent is treeStomp: return true
 	else: return false
+
+func time_convert(time_in_sec):
+	var seconds = time_in_sec%60
+	var minutes = (time_in_sec/60)%60
+	var hours = (time_in_sec/60)/60
+	
+	#returns a string with the format "HH:MM:SS"
+	return "%02d:%02d:%02d" % [hours, minutes, seconds]
