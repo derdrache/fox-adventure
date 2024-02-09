386a8993687f8f8c89216a681cea1970db79f51b
DevDrache <dominik.mast.11@gmail.com>
2024-02-09 11:10:49 +0200

    fix fog

--- level/environment/fog/fog.gd
+++ level/environment/fog/fog.gd
@@ -1,5 +1,37 @@
 extends Area2D
 
+@export var worldTileSet = 0
+@export var tilesWith = 4
+@export var tilesHeight = 4
+
+@onready var tileMap = $TileMap
+@onready var collisionShape = $CollisionShape2D
+
+const tileWorldConverter = {
+	0: Vector2i(3,3),
+	1: Vector2i(3,3),
+	2: Vector2i(1,1),
+	3: Vector2i(1,7),
+	4: Vector2i(1,1),
+	5: Vector2i(1,1),
+	6: Vector2i(7,2),
+	7: Vector2i(1,4),
+}
+
+func _ready():
+	if tilesWith == 0 || tilesHeight == 0: return
+	for iWidth in tilesWith:
+		tileMap.set_cell(0, Vector2(iWidth,0), 0, tileWorldConverter[worldTileSet])
+		for iHeight in tilesHeight:
+			tileMap.set_cell(0, Vector2(iWidth,iHeight), 0, tileWorldConverter[worldTileSet])
+
+	var width = tilesWith * 16
+	var height = tilesHeight * 16
+		
+	var rect = RectangleShape2D.new()
+	$CollisionShape2D.shape = rect	
+	collisionShape.shape.size = Vector2(width, height)
+	collisionShape.position = Vector2(width / 2, height / 2)
 
 
 func _on_body_entered(body):
