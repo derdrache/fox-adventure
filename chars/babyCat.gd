d3d515df3d31fdc267653ca67cfc859fff7b1d6e
DevDrache <dominik.mast.11@gmail.com>
2024-02-11 13:53:39 +0200

    fix movement

--- chars/babyCat.gd
+++ chars/babyCat.gd
@@ -11,7 +11,7 @@ extends CharacterBody2D
 @export var target: Player
 @export var isWaiting = true
 
-const SPEED = 100.0
+var speed = 100
 const JUMP_VELOCITY = -400.0
 
 var animationSprite : AnimatedSprite2D
@@ -43,16 +43,19 @@ func _calculate_velocity():
 	if isWaiting: return
 
 	var targetPosition = target.global_position - Vector2(0, -9)
-	if global_position.distance_to(targetPosition) > 20  * (followPosition +1):
+	if global_position.distance_to(targetPosition) > 20  * followPosition:
 		var direction = (targetPosition - global_position).normalized()
-		velocity = direction * SPEED
+		velocity = direction * speed
 		velocity.y *= 3
 	elif (global_position.y - targetPosition.y) < -2 || (global_position.y - targetPosition.y) > 2:
 		velocity.x = 0
-	else: velocity = Vector2.ZERO
-
+	else: 
+		velocity = Vector2.ZERO
+	
 func _set_animation():
-	if isWaiting: return
+	if isWaiting || target.velocity == Vector2.ZERO: 
+		animationSprite.play("idle")
+		return
 	
 	var targetPosition = target.position - Vector2(0, -9)
 
@@ -81,10 +84,12 @@ func _set_cat_color(catNumber):
 	animationSprite.visible = true
 
 func _catch_up():
+	var catchUpRange = 200
+	
 	if isWaiting: return
 	
-	if global_position.distance_to(target.global_position) > 200:
-		global_position = target.global_position
+	if global_position.distance_to(target.global_position) > catchUpRange:
+		global_position = target.global_position - Vector2(0, -9)
 
 func _on_area_2d_body_entered(body):
 	if body is Player && isWaiting:
