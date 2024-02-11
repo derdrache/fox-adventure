6fe903888d0ac0bca12473f6f0b7b34eeaa15f9c
DevDrache <dominik.mast.11@gmail.com>
2024-02-11 13:54:44 +0200

    fix platforms

--- level/environment/moving_objects/moving_platform.gd
+++ level/environment/moving_objects/moving_platform.gd
@@ -6,14 +6,14 @@ class_name MovingPlatform
 var doMove = false
 
 func _ready():
-	doMove = ! has_node("Area2D")
+	doMove = !has_node("Area2D")
+	
 
 func _physics_process(delta):
 	if doMove: $PathFollow2D.progress += moving_speed * delta
 	
 
-
 func _on_area_2d_body_entered(body):
-	if "Player" in body.name:
+	if "Player" in body.name && !doMove:
 		doMove = true
 		$PathFollow2D.progress_ratio = 0.5
