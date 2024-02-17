53fcf44a191d88227b89e6c0ce869409a28dc1b1
DevDrache <dominik.mast.11@gmail.com>
2024-02-17 12:51:36 +0200

    update

--- ui/settingMenu/SettingMenu.gd
+++ ui/settingMenu/SettingMenu.gd
@@ -1,10 +1,14 @@
 extends Control
 
+@onready var soundEffectSlider = $"TextureRect/VBoxContainer/Sound Effects/SoundEffectSlider"
+@onready var backgroundMusicSlider = $"TextureRect/VBoxContainer/Background Music/BackgroundMusicSlider"
+
+var soundMinValue : int
+
 signal close_window()
 
-var normalSound = 25
-
-
+func _ready():
+	soundMinValue = soundEffectSlider.min_value
 
 func _on_close_button_pressed():
 	emit_signal("close_window")
@@ -20,12 +24,12 @@ func _on_sound_effect_slider_value_changed(value):
 func _change_volume(value, bus):
 	var valueInDB : int
 	
-	if value == 0:
+	if value == soundMinValue:
 		AudioServer.set_bus_mute(AudioServer.get_bus_index(bus), true)
 	else:
 		AudioServer.set_bus_mute(AudioServer.get_bus_index(bus), false)
 		
-		valueInDB = value - normalSound
+		valueInDB = value
 		AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus), valueInDB)	
 		
 	
