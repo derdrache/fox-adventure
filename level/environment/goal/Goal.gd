84605e6f317789346beb066755a64a7b5774ca68
DevDrache <dominik.mast.11@gmail.com>
2024-02-17 12:47:05 +0200

    add sound and transition

--- level/environment/goal/Goal.gd
+++ level/environment/goal/Goal.gd
@@ -1,9 +1,23 @@
 extends Area2D
 
 
+var scoreResource = preload("res://ui/score/Score.tscn")
+var circleTransition = preload("res://ui/CircleTransition.tscn")
+var scoreBoardDone = false
 
 
 func _on_body_entered(body):
 	if body is Player:
+		
 		LevelManager.level_done()
-		get_tree().change_scene_to_file("res://overworld/overWorld.tscn")
+		$AudioStreamPlayer.play()
+		
+		body.circle_transition("out", 3)		
+			
+		await get_tree().create_timer(3).timeout
+		get_tree().change_scene_to_file("res://ui/score/Score.tscn")
+		
+		
+	
+		
+		
