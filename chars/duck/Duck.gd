b884bc65826a90c2f8078719eebb70f947a7fbf7
DevDrache <dominik.mast.11@gmail.com>
2024-02-17 12:40:53 +0200

    update

--- chars/duck/Duck.gd
+++ chars/duck/Duck.gd
@@ -2,6 +2,7 @@ extends Path2D
 
 @onready var sprite = $PathFollow2D/DuckBody/Sprite2D
 @onready var animation = $PathFollow2D/DuckBody/AnimationPlayer
+@onready var animationsSprite = $PathFollow2D/DuckBody/AnimationsSprite
 @onready var pathFollow = $PathFollow2D
 
 @export var flipH = false 
@@ -25,7 +26,7 @@ func _physics_process(delta):
 		pathFollow.progress += moveSpeed * delta
 	else: animation.stop()
 
-	if pathFollow.progress_ratio == 1: 
+	if pathFollow.progress_ratio == 1 && doMove: 
 		doMove = false
 		await _remove_obstacle_animation()
 		emit_signal("interactionDone")	
@@ -36,4 +37,12 @@ func move():
 		
 
 func _remove_obstacle_animation():
-	await get_tree().create_timer(2).timeout
+	animationsSprite.visible = true
+	animationsSprite.play("idea")
+	await get_tree().create_timer(0.5).timeout
+	animationsSprite.visible = false
+	$PathFollow2D/DuckBody/effectAnimations.play("work")
+	await get_tree().create_timer(1.8).timeout
+	animationsSprite.visible = true
+	animationsSprite.play("notes")
+	await get_tree().create_timer(1).timeout
