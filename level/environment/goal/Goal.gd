6d35b7a984ceba22c0f1c0ac011cfbe33c0ff64a
DevDrache <dominik.mast.11@gmail.com>
2024-02-20 09:06:04 +0200

    add sound

--- level/environment/goal/Goal.gd
+++ level/environment/goal/Goal.gd
@@ -8,13 +8,15 @@ var scoreBoardDone = false
 
 func _on_body_entered(body):
 	if body is Player:
-		
+		body.set_physics_process(false)
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("BackgroundMusic"), true)
 		LevelManager.level_done()
 		$AudioStreamPlayer.play()
 		
 		body.circle_transition("out", 3)		
 			
 		await get_tree().create_timer(3).timeout
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("BackgroundMusic"), false)
 		get_tree().change_scene_to_file("res://ui/score/Score.tscn")
 		
 		
