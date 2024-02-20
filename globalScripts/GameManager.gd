f3bac5434763cec1a8d17040ad692f48d5e51659
DevDrache <dominik.mast.11@gmail.com>
2024-02-20 09:04:35 +0200

    add max coins

--- globalScripts/GameManager.gd
+++ globalScripts/GameManager.gd
@@ -12,6 +12,14 @@ var gameStart : Dictionary
 var catMomsDone : Array = [false, false, false, false, false, false, false]
 var backgroundMusicVolumen : int
 var soundEffectsVolumen : int
+var levelMaxGoldCoins = {
+	"1": 34, "2": 58, "3": 31, "4": 76, "5": 79, "6": 51, "7": 68, "8": 42, 
+	"9": 67, "10": 41, "11": 45, "12": 57, "13": 49, "14": 49, "15": 49, 
+	"16": 35, "17": 39, "18": 56, "19": 45, "20": 27, "21": 43, "22": 42, 
+	"23": 48, "24": 73, "25": 55, "26": 48, "27": 67, "28": 52, "29": 48, 
+	"30": 57, "31": 55, "32": 65, "33": 56, "34": 50, "35": 47, "36": 75, 
+	"37": 46, "38": 64, "39": 59, "40": 46, "41": 41, "42": 65
+}
 
 func _ready():	
 	if !levelDetails.is_empty(): return
@@ -23,6 +31,7 @@ func _ready():
 			"redCoins" : 0,
 			"gems": 0,
 			"cats": [false,false,false],
+			"maxGoldCoins": levelMaxGoldCoins[str(i + 1)],
 			"goldCoins": 0
 		})
 		
@@ -33,8 +42,9 @@ func updateLevelData(levelData : LevelDataClass):
 		"redCoins": levelData.redCoins,
 		"gems": levelData.gems,
 		"cats": levelData.cats,
-		"goldCoins": levelData.goldCoins
-	}		
+		"goldCoins": levelData.goldCoins,
+		"maxGoldCoins": levelMaxGoldCoins[str(levelData["level"] + 1)]
+	}	
 		
 func getLevelStatus(level):
 	return levelDetails[level -1];
