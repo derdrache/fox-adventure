1444ba08c0f5af8e9509b06cc74bcee7800e0a55
DevDrache <dominik.mast.11@gmail.com>
2024-02-20 09:06:51 +0200

    add get max coin function

---
+++ level/levels.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+func _ready():
+	_count_gold_coins()
+
+func _count_gold_coins():
+	var maxGoldCoins = 0
+	var goldCoinObjects = $items/goldCoins.get_children()
+	
+	for object in goldCoinObjects:
+		if "GoldCoin" in object.name:
+			maxGoldCoins += 1
+		if "kiste" in object.name:
+			maxGoldCoins += object.goldCoins
+	print(maxGoldCoins)	
