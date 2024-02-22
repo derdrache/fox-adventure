57f5dd1f6bbde1984f062cddc51860b14bd13ef5
DevDrache <dominik.mast.11@gmail.com>
2024-02-22 09:33:46 +0200

    add full done icon

--- ui/levelUI/LevelUI.gd
+++ ui/levelUI/LevelUI.gd
@@ -1,6 +1,12 @@
 extends CanvasLayer
 
 @onready var goldCoinLabel = $Control/MarginContainer/VBoxContainer/HBoxContainer/GoldContainer/Label
+@onready var levelUi = $Control/MarginContainer
+@onready var settingMenu = $Control/SettingMenu
+@onready var backgroundColor = $Control/ColorRect
+@onready var menuContainer = $Control/MenuContainer
+@onready var menuCloseButton = $Control/MarginContainer/VBoxContainer/HBoxContainer/MenuCloseButton
+@onready var menuButton = $Control/MarginContainer/VBoxContainer/HBoxContainer/MenuButton
 
 func _ready():
 	LevelManager.gained_gem.connect(update_gem_display)
@@ -31,6 +37,35 @@ func update_gold_coin_display(coins):
 	goldCoinLabel.text = "x " + str(coins)
 
 
-func _on_texture_button_pressed():
+func _on_menu_button_pressed():
+	menuContainer.visible = true
+	menuCloseButton.visible = true
+	backgroundColor.visible = true
+	
+	menuButton.visible = false
+	
+
+
+func _on_setting_menu_close_window():
+	menuContainer.visible = true
+	
+	settingMenu.visible = false
+
+
+func _on_exit_button_pressed():
 	LevelManager.reset_all_stats()
 	get_tree().change_scene_to_file("res://overworld/overWorld.tscn")
+
+
+func _on_setting_button_pressed():
+	settingMenu.visible = true
+	menuContainer.visible = false
+
+
+func _on_menu_close_button_pressed():
+	menuContainer.visible = false
+	menuCloseButton.visible = false
+	backgroundColor.visible = false
+	
+	menuButton.visible = true
+	levelUi.visible = true
