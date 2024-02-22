614f124c58f85169bc54a71ab81c7487c5b17e79
DevDrache <dominik.mast.11@gmail.com>
2024-02-22 09:36:04 +0200

    add variable texture

--- ui/short_message_box.gd
+++ ui/short_message_box.gd
@@ -3,6 +3,12 @@ extends Control
 @onready var messageContainer = $MessageContainer
 @onready var messageIconContainer = $MarginContainer
 
+@export var messageTexture : Texture
+
+
+func _process(delta):
+	if messageTexture: $MarginContainer/VBoxContainer/TextureRect.texture = messageTexture
+
 func flip_box(boolean):
 	messageContainer.scale.x *= -1
 	
