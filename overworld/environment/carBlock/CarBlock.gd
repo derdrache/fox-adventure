f1f14d44a73b59f8346ff0f2698540d284a6e3bf
DevDrache <dominik.mast.11@gmail.com>
2024-02-22 09:33:04 +0200

    add duck drive car animation

--- overworld/environment/carBlock/CarBlock.gd
+++ overworld/environment/carBlock/CarBlock.gd
@@ -2,6 +2,7 @@ extends Path2D
 
 @onready var pathFollow = $PathFollow2D
 @onready var sprite = $PathFollow2D/CarBlockBody/Sprite2D
+@onready var animatedSprite = $PathFollow2D/CarBlockBody/AnimatedSprite2D
 
 var doMove = false
 var moveSpeed = 50
@@ -13,6 +14,8 @@ func _physics_process(delta):
 func _on_area_2d_body_entered(body):
 	if "Duck" in body.name:
 		await get_tree().create_timer(2).timeout
-		sprite.flip_h = body.flipH
-		sprite.visible = true
+		sprite.visible = false
+		body.queue_free()
+		animatedSprite.visible = true
+		animatedSprite.play("front")
 		doMove = true	
