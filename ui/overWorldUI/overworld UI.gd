47493ac335021bdb7f238e96e2c4a1c6400b5d81
DevDrache <dominik.mast.11@gmail.com>
2024-02-22 09:35:27 +0200

    fix overworld ui

--- ui/overWorldUI/overworld UI.gd
+++ ui/overWorldUI/overworld UI.gd
@@ -8,12 +8,11 @@ extends CanvasLayer
 
 
 func _on_close_button_pressed():
-	controlNode.anchors_preset = 10
-	menuButton.visible = true
-	backgroundRect.visible = false
 	closeButtonRect.visible = false
-	menuSelection.visible = false	
+	backgroundRect.visible = false
+	menuSelection.visible = false
 
+	menuButton.visible = true	
 
 func _on_menu_button_pressed():
 	controlNode.anchors_preset = 15
@@ -24,19 +23,21 @@ func _on_menu_button_pressed():
 	
 
 
-func _on_exit_button_pressed():
-	GameManager.gameNumber = 0
-	get_tree().change_scene_to_file("res://ui/startMenu/startMenu.tscn")
 
 func _on_setting_button_pressed():
 	$Control/SettingMenu.visible = true
 	layer = 10
-	menuSelection.visible = false
 	menuSelection.visible = false
 	closeButtonRect.visible = false
 	menuButton.visible = true
 
 
 func _on_setting_menu_close_window():
-	backgroundRect.visible = false
-	layer = -1
+	layer = 0
+	closeButtonRect.visible = true
+	menuSelection.visible = true
+	menuButton.visible = false
+
+
+func _on_exit_button_pressed():
+	get_tree().change_scene_to_file("res://ui/startMenu/startMenu.tscn")
