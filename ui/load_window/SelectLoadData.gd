aadb1ecc873dc49f8510adc776a3a376161a5cd5
DevDrache <dominik.mast.11@gmail.com>
2024-02-22 09:34:04 +0200

    fix save and load system

--- ui/load_window/SelectLoadData.gd
+++ ui/load_window/SelectLoadData.gd
@@ -2,7 +2,7 @@ extends Control
 
 var saveFiles : Array
 var saveFileContainerNode = preload("res://ui/load_window/save_file_container.tscn")
-
+var nextFreeSaveSlot = -1
 
 func _ready():
 	saveFiles = _get_save_files()
@@ -18,20 +18,22 @@ func _get_save_files():
 	
 		if FileAccess.file_exists(savePath):
 			data = JSON.parse_string(file.get_line())
-			saveFiles.append(data)
+			saveFiles.append({"fileNumber": i, "data": data})
+		else: if nextFreeSaveSlot < 0 :nextFreeSaveSlot = i
 	
 	return saveFiles
 	
 func _create_save_file_container():
 	for i in len(saveFiles):
 		var saveFileContainer = saveFileContainerNode.instantiate()
-		saveFileContainer.gameNumber = i
-		saveFileContainer.saveData = saveFiles[i]
+		var saveFileNumber = saveFiles[i]["fileNumber"]
+		
+		saveFileContainer.gameNumber = saveFiles[i]["fileNumber"]
+		saveFileContainer.saveData = saveFiles[i]["data"]
 		$VBoxContainer/HBoxContainer.add_child(saveFileContainer)
 
 func _on_new_game_button_pressed():
-	var savedGamesCount = len(saveFiles)
-	GameManager.gameNumber = savedGamesCount
+	GameManager.gameNumber = nextFreeSaveSlot
 	GameManager.gameStart = Time.get_datetime_dict_from_system()
 	get_tree().change_scene_to_file("res://overworld/overWorld.tscn")
 
