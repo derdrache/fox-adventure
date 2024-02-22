eb5f1f7e0f93f36e0a4b73446c5a22dc1ddc5da9
DevDrache <dominik.mast.11@gmail.com>
2024-02-22 09:33:14 +0200

    add full done indicator

--- overworld/environment/levelTile/OW_level_tile.gd
+++ overworld/environment/levelTile/OW_level_tile.gd
@@ -2,7 +2,17 @@ extends Area2D
 class_name EnterLevelTile
 
 @export var levelDetails : Control
+@export var levelFullDone = false
+@export var withLevelDoneIcon = true
 
+
+func _ready():
+	if !withLevelDoneIcon: levelFullDone = false
+	if levelFullDone: $Sprite2D.visible = true
+
+func set_level_full_done():
+	levelFullDone = true
+	$Sprite2D.visible = true
 
 func _on_body_entered(body):
 	if body is PlayerOverworld && levelDetails:
