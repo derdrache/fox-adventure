82a37fa3f4ab7a13aa6aff545ef557bc2bc92d77
DevDrache <dominik.mast.11@gmail.com>
2024-02-23 14:09:42 +0200

    add multi textures

--- ui/short_message_box.gd
+++ ui/short_message_box.gd
@@ -4,10 +4,17 @@ extends Control
 @onready var messageIconContainer = $MarginContainer
 
 @export var messageTexture : Texture
-
+@export var messageTexture2 : Texture
 
 func _process(delta):
 	if messageTexture: $MarginContainer/VBoxContainer/TextureRect.texture = messageTexture
+	if messageTexture2:
+		$MarginContainer.position.y = -29
+		$MarginContainer/VBoxContainer/TextureRect2.texture = messageTexture2
+		$MarginContainer/VBoxContainer/TextureRect2.visible = true
+		
+		$MessageContainer.visible = false
+		$MessageContainer2.visible = true
 
 func flip_box(boolean):
 	messageContainer.scale.x *= -1
