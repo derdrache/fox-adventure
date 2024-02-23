fda5ca930fc4d75427825146fcc9679d6549e4e1
DevDrache <dominik.mast.11@gmail.com>
2024-02-23 14:09:54 +0200

    add gamepad control

--- ui/startMenu/startMenu.gd
+++ ui/startMenu/startMenu.gd
@@ -1,6 +1,9 @@
 extends Control
 
 
+func _ready():
+	$VBoxContainer/VBoxContainer/StartButton.grab_focus()
+
 func _on_start_button_pressed():
 	Utils.load_game("settings")
 	get_tree().change_scene_to_file("res://ui/load_window/SelectLoadData.tscn")
@@ -8,3 +11,4 @@ func _on_start_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
