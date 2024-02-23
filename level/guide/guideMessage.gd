779102b5e169ce52b703bc342a1d071f184cdb3b
DevDrache <dominik.mast.11@gmail.com>
2024-02-23 14:07:10 +0200

    add guide message

--- level/guide/guideMessage.gd
+++ level/guide/guideMessage.gd
@@ -1,17 +1,18 @@
-extends Node2D
+extends Area2D
 
 @export var messageTexture : Texture
+@export var messageTexture2 : Texture
 
 func _ready():
 	if messageTexture: 
 		$ShortMessageBox.messageTexture = messageTexture
+		$ShortMessageBox.messageTexture2 = messageTexture2
 
-
-func _on_area_2d_body_entered(body):
-	if "Player" in body.name:
-		$ShortMessageBox.visible = true
-
-
-func _on_area_2d_body_exited(body):
+func _on_body_exited(body):
 	if "Player" in body.name:
 		$ShortMessageBox.visible = false
+
+
+func _on_body_entered(body):
+	if "Player" in body.name:
+		$ShortMessageBox.visible = true
