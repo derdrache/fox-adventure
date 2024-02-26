449e13ec97301505ab5192512a2210f2696631fd
DevDrache <dominik.mast.11@gmail.com>
2024-02-26 13:24:41 +0200

    remove

--- level/player/Camera2D.gd
+++ level/player/Camera2D.gd
@@ -2,13 +2,15 @@ extends Camera2D
 
 @export var tileMap: TileMap
 
-@onready var levelTileMap = get_node("../../TileMap")
+@onready var levelTileMap : TileMap = get_node("../../TileMap")
 
 func _ready():
 	var mapRect = levelTileMap.get_used_rect()
+	print(mapRect)
 	var tileSize = levelTileMap.cell_quadrant_size
+	print(tileSize)
 	var worldSizeInPixel = mapRect.size * tileSize
-	#limit_left = 0
-	#limit_top = 100
-	#limit_right = worldSizeInPixel.x
+	print(worldSizeInPixel)
+	limit_left = 0 
+	limit_right = worldSizeInPixel.x + 100
 
