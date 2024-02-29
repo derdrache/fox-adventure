d05d8f72fa075db1a4c69064ebeaa4a4faffd601
DevDrache <dominik.mast.11@gmail.com>
2024-02-29 08:40:54 +0200

    update

--- ui/load_window/save_file_container.gd
+++ ui/load_window/save_file_container.gd
@@ -6,29 +6,36 @@ extends PanelContainer
 @export var playTimeLabel :Label
 @export var gameDoneProcentLabel : Label
 
+@onready var headerLabel = $VBoxContainer/Header/PanelContainer/HBoxContainer/LoadCount/PanelContainer/Label
+
 var touchCanFire = true
+var isLoaded = false
+
+func _ready():
+	if DisplayServer.is_touchscreen_available(): 
+		focus_mode = Control.FOCUS_NONE
 
 func _process(_delta):
-	print(has_focus())
 	if has_focus():
-		print("test")
-		#var styleBox : StyleBoxFlat = get_theme_stylebox("panel")
-		#styleBox.border_color = Color(1,1,1)
-		#add_theme_stylebox_override("panel", styleBox)
-	$VBoxContainer/Header/PanelContainer/HBoxContainer/LoadCount/PanelContainer/Label.text = str(gameNumber +1)
-	_set_procent_clear()
-	_set_play_time()
-	_set_load_file_image()
+		var styleBox : StyleBoxFlat = get_theme_stylebox("panel").duplicate()
+		styleBox.border_color = Color(1,1,1)
+		add_theme_stylebox_override("panel", styleBox)
+	else:
+		var styleBox : StyleBoxFlat = get_theme_stylebox("panel").duplicate()
+		styleBox.border_color = Color(0.647, 0.671, 0.718)
+		add_theme_stylebox_override("panel", styleBox)	
+		
+	headerLabel.text = str(gameNumber +1)
+	
+	if !isLoaded:
+		isLoaded = true
+		_set_procent_clear()
+		_set_play_time()
+		_set_load_file_image()
 
 func _set_procent_clear():
 	var allLevel : Array = saveData["levelDetails"]
-	var levelDone = 0
-	
-	for level in allLevel:
-		if level["isFinished"]: levelDone += 1
-	
-	var procentDone = levelDone * 100 / GameManager.LEVEL_COUNT
-	gameDoneProcentLabel.text = str(procentDone) + " %"
+	gameDoneProcentLabel.text = str(GameManager.procent_done(allLevel)) + " %"
 
 func _set_play_time():
 	var playTimeSeconds : int = saveData["playTimeSeconds"]
@@ -39,15 +46,7 @@ func _set_load_file_image():
 	var texture = ImageTexture.create_from_image(image)
 	loadingTexture.texture = texture  
 
-func _on_delete_button_pressed():
-	$reallyDeleteContainer.visible = true
-		
-	touchCanFire = false
-	$VBoxContainer.visible = false
-	$deleteButton.visible = false
-
-
-func _on_start_game_touch_button_released():
+func _start_game():
 	if !touchCanFire: return
 	
 	GameManager.gameNumber = gameNumber
@@ -55,6 +54,21 @@ func _on_start_game_touch_button_released():
 	Utils.load_game()
 	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file("res://overworld/overWorld.tscn")
+
+func _input(event):
+	if event.is_action_pressed("ui_accept") && has_focus():
+		_start_game()
+
+func _on_delete_button_pressed():
+	$reallyDeleteContainer.visible = true
+		
+	touchCanFire = false
+	$VBoxContainer.visible = false
+	$MarginContainer/deleteButton.visible = false
+
+
+func _on_start_game_touch_button_released():
+	_start_game()
 
 
 func _on_yes_button_pressed():
@@ -67,4 +81,4 @@ func _on_no_button_pressed():
 		
 	touchCanFire = true
 	$VBoxContainer.visible = true
-	$deleteButton.visible = true
+	$MarginContainer/deleteButton.visible = true
